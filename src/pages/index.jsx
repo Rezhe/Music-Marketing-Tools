86b5951bb506fa0365a8f47ca99d74312008dd0a
rezhe <rzwang.ca@gmail.com>
2019-04-17 03:11:06 -0400

    That's just how I roll

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,17 +15,12 @@ const IndexPage = () => (
     <Container>
       <Title />
       <Flex width={[1, 1, 1]} flexWrap="wrap">
-        <Box width={[1, 1, 2 / 3]}>
+        <Box width={[1, 1, 2.2 / 3]}>
           <AppleMusic />
           <BandCamp />
           <Spotify />
           <NetEase />
           <QQmusic />
-        </Box>
-        <Box ml={18} width={[1, 1, 17 / 54]}>
-          <a href="https://www.atlassian.com/software/jira/whats-new-overview?utm_source=buy-sell-ads&utm_medium=display&utm_campaign=jira_imc-newjira-19_all-eng_buy-sell-ads_carbon_dev-circle&utm_content=jira_imc-newjira-19_all-eng_buy-sell-ads_carbon_dev-circle_display_jira-logo-130x100-adcopy1_general_1x1&landingpage=general">
-            <Image src="https://raw.githubusercontent.com/Rezhe/Music-Marketing-Tools/master/src/assets/carbonAd.png" />
-          </a>
         </Box>
       </Flex>
     </Container>
